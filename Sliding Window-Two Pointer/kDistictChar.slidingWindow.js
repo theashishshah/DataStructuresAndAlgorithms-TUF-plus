593ac9a862cdeb1ac3aeddb54e38253d51f2adba
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-17 02:01:43 +0530

    Solve: kDistictChar in a string using TC: O(N + N), SC: O(N)

--- Sliding Window-Two Pointer/kDistictChar.slidingWindow.js
+++ Sliding Window-Two Pointer/kDistictChar.slidingWindow.js
@@ -15,3 +15,28 @@ function kDistinctChar(inputString, k) {
     }
     return maxLength;
 }
+
+
+kDistinctChar(inputString, k) {
+    if (!inputString) return 0;
+    const charSet = new Set(inputString);
+    if (charSet.size === 1) return inputString.length;
+    charSet.clear();
+    let left = 0;
+    let right = 0;
+    let maxLength = 0;
+    const map = new Map();
+    while (right < inputString.length) {
+      map.set(inputString[right], (map.get(inputString[right]) || 0) + 1);
+      while (map.size > k) {
+        let leftValue = map.get(inputString[left]);
+        if (leftValue <= 1) map.delete(inputString[left]);
+        else map.set(inputString[left], leftValue - 1);
+        left++;
+      }
+      maxLength = Math.max(maxLength, right - left + 1);
+      right++;
+    }
+    return maxLength;
+  }
+  
