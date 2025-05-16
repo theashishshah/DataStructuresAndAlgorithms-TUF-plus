9ecdb1516fae881fefbd10eef3f53eb75f7b7d14
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-17 00:09:06 +0530

    Solve: optimized solution of longest one in an binary array

--- Sliding Window-Two Pointer/longestOne.slidingWindow.js
+++ Sliding Window-Two Pointer/longestOne.slidingWindow.js
@@ -18,3 +18,25 @@ function longestOnes(nums, k) {
     return maxLength;
 }
 console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]));
+
+
+function longestOnes (nums, k) {
+    // TC: O(N + N) SC: O(1)
+    if (!nums) return 0;
+    let maxLength = 0;
+    let currentZeros = 0;
+    let left = 0;
+    let right = 0;
+    while (right < nums.length) {
+      if (nums[right] === 0) currentZeros++;
+      if (currentZeros > k) {
+        if (nums[left] === 0) currentZeros--;
+        left++;
+        right++;
+      } else {
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+      }
+    }
+    return maxLength;
+};
