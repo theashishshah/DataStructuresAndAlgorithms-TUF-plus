30bef01a817512918793dff233a0d96206e33e13
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-21 01:30:50 +0530

    solve: find the height of the BT

--- Binary Tree/maxDepth.js
+++ Binary Tree/maxDepth.js
@@ -1,6 +1,18 @@
-class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = this.right = null;
+function  maxDepth(root) {
+        if(!root) return 0
+        
+        // using level order traversal
+        const q = []
+        let height = 0
+        q.push(root)
+        while(q.length){
+            const size = q.length
+            height++
+            for(let i = 0; i < size; i++){
+                const node = q.shift()
+                if(node.left) q.push(node.left)
+                if(node.right) q.push(node.right)
+            }
+        }
+        return height
     }
-}
