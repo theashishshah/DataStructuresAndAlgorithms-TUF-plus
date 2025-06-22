01bedbb654eb8af1e08f0fbdce72bde5822c052e
Ashish Shah <eiepixxstuff@gmail.com>
2025-06-22 15:41:46 +0530

    implement BST iterator optimized one.

--- Binary Search Tree/BSTiterator.BST.js
+++ Binary Search Tree/BSTiterator.BST.js
@@ -21,3 +21,28 @@ class BSTIterator {
         return this.inorder[++this.index]
     }
 }
+
+
+/// BST iterator optimized one
+class BSTIteratorOptimized {
+    constructor(root) {
+        this.stack = []
+        this._addAllLefts(root)
+    }
+
+    _addAllLefts(root) {
+        if (!root) return
+        this.stack.push(root)
+        this._addAllLefts(root.left)
+    }
+
+    hasNext() {
+        return this.stack.length > 0
+    }
+
+    next() {
+        const temp = this.stack.pop()
+        this._addAllLefts(temp.right)
+        return temp.data
+    }
+}
