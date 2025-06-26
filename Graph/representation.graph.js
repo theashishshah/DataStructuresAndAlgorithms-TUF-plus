dd061f0f49b8f869152cc4d35e13f29d9af7c4ea
Ashish Shah <eiepixxstuff@gmail.com>
2025-06-26 21:00:28 +0530

    weighted graph representation

--- Graph/representation.graph.js
+++ Graph/representation.graph.js
@@ -1,8 +1,13 @@
 // using adj matrix
+
 const adj = Array.from({ length: 5 }, () => Array(3).fill(0))
 for (cosnt[u, v] of edges) {
     adj[u][v] = 1
     adj[v][u] = 1
 }
 
-// using adj list
+const adjWeighted = Array.from({ length: 5 }, () => Array(3).fill(0))
+for (cosnt[u, v, w] of edges) {
+    adjWeighted[u][v] = w
+    adjWeighted[v][u] = w
+}
