0b2fecc52b519a42019a9df60aff6524b7d36175
Ashish Shah <eiepixxstuff@gmail.com>
2025-06-27 22:37:37 +0530

    number of enclaves on leetcode

--- Graph/numberOfEnclaves.GRAPH.js
+++ Graph/numberOfEnclaves.GRAPH.js
@@ -50,3 +50,51 @@ class Solution {
 
     }
 }
+
+//leetcode
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var numEnclaves = function (grid) {
+    const rows = grid.length
+    const cols = grid[0].length
+
+    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false))
+
+    const queue = []
+    // now get the boundary elements and mark them visited
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if ((i === 0 || j === 0 || j === cols - 1 || i === rows - 1) && grid[i][j] === 1) {
+                queue.push([i, j])
+                visited[i][j] = true
+            }
+        }
+    }
+
+    // traverse to each nodes where you can reach via these folks
+    while (queue.length) {
+        const [row, col] = queue.shift()
+        for (const [dx, dy] of directions) {
+            const nrow = row + dx
+            const ncol = col + dy
+            if (nrow < rows && ncol < cols &&
+                nrow >= 0 && ncol >= 0 && grid[nrow][ncol] === 1 && !visited[nrow][ncol]
+            ) {
+                visited[nrow][ncol] = true
+                queue.push([nrow, ncol])
+            }
+        }
+    }
+
+    let count = 0
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] && !visited[i][j]) count++
+        }
+    }
+    return count
+};
