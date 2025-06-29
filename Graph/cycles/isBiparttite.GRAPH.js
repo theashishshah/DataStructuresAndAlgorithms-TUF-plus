531624319876347eaa383cd70816c98a648a8db7
Ashish Shah <eiepixxstuff@gmail.com>
2025-06-29 17:41:56 +0530

    is graph bipartite bfs

--- Graph/cycles/isBiparttite.GRAPH.js
+++ Graph/cycles/isBiparttite.GRAPH.js
@@ -22,3 +22,32 @@ var isBipartite = function (graph) {
     }
     return true
 };
+
+
+// bfs
+class Solution {
+    findBi(src, color, graph) {
+        color[src] = 0
+        const queue = []
+        queue.push(src)
+        while (queue.length) {
+            const node = queue.shift()
+            for (const nnode of graph[node]) {
+                if (color[nnode] === -1) {
+                    color[nnode] = 1 - color[node]
+                    queue.push(nnode)
+                } else if (color[nnode] === color[node]) return true
+            }
+        }
+        return false
+    }
+    isBipartite(V, adj) {
+        const color = new Array(V).fill(-1)
+        for (let i = 0; i < V; i++) {
+            if (color[i] === -1) {
+                if (this.findBi(i, color, adj)) return false
+            }
+        }
+        return true
+    }
+}
