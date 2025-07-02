9de7ad9a03966a23e6db8d86e583be44f38d3c39
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-03 05:29:08 +0530

    course schedule 1 using topo sort kahn's algorithm

--- Graph/cycles/courseSchedule1.GRAPH.js
+++ Graph/cycles/courseSchedule1.GRAPH.js
@@ -32,3 +32,42 @@ class Solution {
         return true
     }
 }
+
+
+// using topo sort
+class Solution {
+    canFinish(n, arr) {
+        // using topo sorting
+        // create graph for the list
+        const graph = Array.from({ length: n }, () => [])
+        for (const node of arr) {
+            const [to, fromm] = node
+            graph[fromm].push(to)
+        }
+
+        // find inDegree for all
+        const indegree = new Array(n).fill(0)
+        for (let i = 0; i < n; i++) {
+            for (const nnode of graph[i]) {
+                indegree[nnode]++
+            }
+        }
+
+        const queue = []
+        // insert all the nodes who's indegree is 0
+        for (let i = 0; i < n; i++) {
+            if (indegree[i] === 0) queue.push(i)
+        }
+
+        const ans = []
+        while (queue.length) {
+            const node = queue.shift()
+            ans.push(node)
+            for (const nnode of graph[node]) {
+                indegree[nnode]--
+                if (indegree[nnode] === 0) queue.push(nnode)
+            }
+        }
+        return ans.length !== n ? false : true
+    }
+}
