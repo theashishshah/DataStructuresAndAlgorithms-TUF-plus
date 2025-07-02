e9a34e3b3680efcdd68ed8ce5aa83d6a27d7c589
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-03 04:40:09 +0530

    eventual safe node using bfs kahn's algorithm

--- Graph/cycles/findEventualSafe.GRAPH.js
+++ Graph/cycles/findEventualSafe.GRAPH.js
@@ -29,3 +29,44 @@ class Solution {
         return ans.sort((a, b) => a - b)
     }
 }
+
+
+// using bfs
+class Solution {
+    eventualSafeNodes(V, adj) {
+
+        // TC: O(N + E) + 
+        const graph = Array.from({ length: V }, () => [])
+        // reverse the graph
+        for (let i = 0; i < V; i++) {
+            for (const nnode of adj[i]) {
+                graph[nnode].push(i)
+            }
+        }
+
+        const inDegree = new Array(V).fill(0)
+        for (let i = 0; i < V; i++) {
+            for (const nnode of graph[i]) {
+                inDegree[nnode]++
+            }
+        }
+
+        // insert the nodes who are having inDegree 0
+        const queue = []
+        for (let i = 0; i < V; i++) {
+            if (inDegree[i] === 0) queue.push(i)
+        }
+
+        // start traversing bfs who's inDegree is 0
+        const ans = []
+        while (queue.length) {
+            const node = queue.shift()
+            ans.push(node)
+            for (const nnode of graph[node]) {
+                inDegree[nnode]--
+                if (inDegree[nnode] === 0) queue.push(nnode)
+            }
+        }
+        return ans.sort((a, b) => a - b)
+    }
+}
