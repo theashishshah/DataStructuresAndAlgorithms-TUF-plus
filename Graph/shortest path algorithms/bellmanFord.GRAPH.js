bc4edd5104e2e2e7e87a2dcef86f2e3833a95d7e
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-08 22:07:30 +0530

    .

--- Graph/shortest path algorithms/bellmanFord.GRAPH.js
+++ Graph/shortest path algorithms/bellmanFord.GRAPH.js
@@ -1,23 +1,27 @@
 class Solution {
     bellman_ford(n, edges, source) {
-      const dist = Array(n).fill(1e9);
-      dist[source] = 0;
-      for (let i = 0; i < n - 1; i++) {
-        for (const [from, to, weight] of edges) {
-          if (dist[from] !== 1e9 && dist[from] + weight < dist[to]) {
-            dist[to] = dist[from] + weight;
-          }
+        const INF = 1e9
+        const dist = new Array(n).fill(INF)
+        dist[source] = 0
+        for (let i = 0; i < n - 1; i++) {
+            let anyChange = false
+            for (const [from, to, weight] of edges) {
+                if (dist[from] !== INF && dist[from] + weight < dist[to]) {
+                    dist[to] = dist[from] + weight
+                    anyChange = true
+                }
+            }
+
+            if (!anyChange) break // if there is no anyChange
         }
-      }
-  
-      // now you've found the sortest path
-      // try to get negative cycle
-  
-      for (const [from, to, weight] of edges) {
-        if (dist[from] !== 1e9 && dist[from] + weight < dist[to]) return [-1];
-      }
-  
-      return dist;
+
+        for (const [from, to, weight] of edges) {
+            if (dist[from] !== INF && dist[from] + weight < dist[to]) {
+                return [-1] // negative cycle detected
+            }
+        }
+
+        return dist
     }
-  }
-  
+}
+
