77147e03b122092f18bf9658911f2adb126dfa47
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-10 05:41:00 +0530

    better largest element in an array

--- Array/Fundamentals/secondLargest.js
+++ Array/Fundamentals/secondLargest.js
@@ -10,3 +10,28 @@ class Solution {
         return -1
     }
 } // brute force solution
+
+
+// better: O(2N)
+class Solution {
+    secondLargestElement(nums) {
+        // better 
+        // TC: O(N + N) = O(2N)
+        // SC: O(1)
+        if (nums.length === 1) return -1
+        let largest = -Infinity
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) largest = nums[i]
+        }
+
+        let second = -Infinity
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] > second && nums[i] !== largest) {
+                second = nums[i]
+            }
+        }
+
+        if (second === -Infinity) return -1
+        else return second
+    }
+}
