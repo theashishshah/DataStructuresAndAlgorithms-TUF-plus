43176b7c4386ada78f5b40a9c0a96b03cac4e15f
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-12 05:45:26 +0530

    rotate array optimal

--- Array/Fundamentals/leftRotateArrayByOne.js
+++ Array/Fundamentals/leftRotateArrayByOne.js
@@ -11,3 +11,28 @@ class Solution {
         return nums
     }
 }
+
+// better
+class Solution {
+    rotateArray(nums, k) {
+        if (k <= 0) return nums
+        k = k % nums.length
+
+        const temp = []
+        for (let i = 0; i < k; i++) {
+            temp.push(nums[i])
+        }
+
+        for (let i = k; i < nums.length; i++) {
+            nums[i - k] = nums[i]
+        }
+
+        let j = 0
+        for (let i = nums.length - k; i < nums.length; i++) {
+            nums[i] = temp[j]
+            j++
+        }
+
+        return nums
+    }
+}
