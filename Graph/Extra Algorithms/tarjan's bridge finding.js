29083f736fc813411c4a6fb1d4316469c1383360
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-14 01:35:17 +0530

    tarjan's algorithm leedcode version

--- Graph/Extra Algorithms/tarjan's bridge finding.js
+++ Graph/Extra Algorithms/tarjan's bridge finding.js
@@ -35,3 +35,45 @@ class Solution {
         return ans
     }
 }
+
+
+// leetcode version
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number[][]}
+ */
+
+var criticalConnections = function (n, edges) {
+    let timer = 1
+    // step 1: create the graph
+    const graph = Array.from({ length: n }, () => [])
+    for (const [from, to] of edges) {
+        graph[from].push(to)
+        graph[to].push(from)
+    }
+
+    // initialize important arrays to keep track
+    const visited = new Array(n).fill(false)
+    const tin = new Array(n).fill(0)
+    const low = new Array(n).fill(0)
+    const ans = []
+
+    function dfs(node, parent) {
+        visited[node] = true
+        tin[node] = low[node] = timer++
+        for (const nextNode of graph[node]) {
+            if (nextNode === parent) continue
+            if (!visited[nextNode]) {
+                dfs(nextNode, node)
+                low[node] = Math.min(low[node], low[nextNode])
+                if (low[nextNode] > tin[node]) ans.push([node, nextNode])
+            } else {
+                low[node] = Math.min(low[node], tin[nextNode])
+            }
+        }
+    }
+    dfs(0, -1)
+    return ans
+
+};
