8bf864e581301a81db5d5dbb3528db9dddf18d77
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-16 22:35:29 +0530

    brute force of finding missing number

--- Array/Logic Building/missingNumber.js
+++ Array/Logic Building/missingNumber.js
@@ -1,3 +1,19 @@
+class Solution {
+    missingNumber(nums) {
+        // TC: O(n*n)
+        // sc: o(1)
+        for (let i = 0; i <= nums.length; i++) {
+            let flag = false
+            for (let j = 0; j < nums.length; j++) {
+                if (nums[j] === i) {
+                    flag = true
+                }
+            }
+            if (!flag) return i
+        }
+    }
+}
+
 var missingNumber = function (nums) {
     // TC: O(nlogn) + O(n)
     // SC: O(1)
