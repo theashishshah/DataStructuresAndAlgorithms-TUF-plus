f086c8f05ad3adff7eb2f7cb18d3bf896cfd87d7
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-20 14:25:20 +0530

    using memoization

--- Dynamic Programming/1D/frogJumpWithKDistance.dp.js
+++ Dynamic Programming/1D/frogJumpWithKDistance.dp.js
@@ -18,3 +18,25 @@ class Solution {
     }
 }
 
+// using memoization
+class Solution {
+    minCost(heights, n, k, memo) {
+        if (n <= 0) return 0
+        if (memo[n] !== -1) return memo[n]
+        let localMin = Infinity
+        for (let i = 1; i <= k; i++) {
+            if (n - i >= 0) {
+                const jump = this.minCost(heights, n - i, k, memo) + Math.abs(heights[n] - heights[n - i])
+                localMin = Math.min(localMin, jump)
+            }
+        }
+        return memo[n] = localMin
+    }
+    frogJump(heights, k) {
+        const n = heights.length
+        if (n === 1) return 0
+        const memo = new Array(n).fill(-1)
+
+        return this.minCost(heights, n - 1, k, memo)
+    }
+}
