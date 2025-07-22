78104a880ed55aa8de40b156cc80e1a7607d2a30
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-22 09:08:33 +0530

    max sum of non adjacent elements using memoization

--- Dynamic Programming/1D/maxSum.dp.js
+++ Dynamic Programming/1D/maxSum.dp.js
@@ -11,3 +11,26 @@ class Solution {
         return maxSum(nums.length - 1)
     }
 }
+
+
+// using memoization
+class Solution {
+    maxSum(index, nums, memo) {
+        if (index === 0) return nums[index]
+        if (index < 0) return 0
+
+        if (memo[index] !== -1) return memo[index]
+        const pick = nums[index] + this.maxSum(index - 2, nums, memo)
+        const notPick = this.maxSum(index - 1, nums, memo)
+        return memo[index] = Math.max(pick, notPick)
+    }
+    nonAdjacent(nums) {
+        // tc: o(n)
+        // sc: o(n) + o(n)
+        const n = nums.length
+        if (n === 1) return nums[0]
+
+        const memo = new Array(n).fill(-1)
+        return this.maxSum(n - 1, nums, memo)
+    }
+}
