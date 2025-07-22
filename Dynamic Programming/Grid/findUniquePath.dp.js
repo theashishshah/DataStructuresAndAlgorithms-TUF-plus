83fe51e5fe0ae4b65c34ea18887b28e1fc5d96bf
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-23 03:56:35 +0530

    finding unique path using memoization method

--- Dynamic Programming/Grid/findUniquePath.dp.js
+++ Dynamic Programming/Grid/findUniquePath.dp.js
@@ -22,3 +22,29 @@ class Solution {
         return this.uniquePathsFinder(0, 0, m, n, directions)
     }
 }
+
+class Solution {
+    uniquePathsFinder(row, col, m, n, directions, memo) {
+        if (row === 0 && col === 0) return 1
+
+        if (memo[row][col] !== -1) return memo[row][col]
+
+        let count = 0
+        // explore the neighbours
+        for (const [dx, dy] of directions) {
+            const nrow = dx + row
+            const ncol = dy + col
+            // check if you can go either bottom or right
+            if (nrow >= 0 && ncol >= 0) {
+                count += this.uniquePathsFinder(nrow, ncol, m, n, directions, memo)
+            }
+        }
+
+        return memo[row][col] = count
+    }
+    uniquePaths(m, n) {
+        const memo = Array.from({ length: m }, () => Array(n).fill(-1))
+        const directions = [[-1, 0], [0, -1]]
+        return this.uniquePathsFinder(m - 1, n - 1, m, n, directions, memo)
+    }
+}
