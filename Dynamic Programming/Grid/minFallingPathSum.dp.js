3f9f1c1f3e38a48e26f44809c01a037fcf41fc1c
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-23 06:58:30 +0530

    solve using memoization method

--- Dynamic Programming/Grid/minFallingPathSum.dp.js
+++ Dynamic Programming/Grid/minFallingPathSum.dp.js
@@ -25,3 +25,40 @@ class Solution {
         return min
     }
 }
+
+
+// using memoization
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+
+function getMinPath(row, col, matrix, memo) {
+    const m = matrix.length
+    const n = matrix[0].length
+    // handle when you go off the matrix
+    if (col < 0 || col >= n || row >= m) return Infinity
+
+
+    if (row === m - 1) return matrix[row][col]
+    if (memo[row][col] !== -1) return memo[row][col]
+
+
+    const leftBtm = getMinPath(row + 1, col - 1, matrix, memo)
+    const btm = getMinPath(row + 1, col, matrix, memo)
+    const rightBtm = getMinPath(row + 1, col + 1, matrix, memo)
+    return memo[row][col] = matrix[row][col] + Math.min(leftBtm, btm, rightBtm)
+
+}
+var minFallingPathSum = function (matrix) {
+    const m = matrix.length
+    const n = matrix[0].length
+    if (m === 1) return Math.min(...matrix[0])
+
+    const memo = Array.from({ length: m }, () => Array(n).fill(-1))
+    let result = Infinity
+    for (let i = 0; i < n; i++) {
+        result = Math.min(result, getMinPath(0, i, matrix, memo))
+    }
+    return result
+};
