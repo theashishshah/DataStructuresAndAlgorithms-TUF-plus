273510e57e1c2718100ad8f84699b5d902985701
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-23 09:23:25 +0530

    using memoization

--- Dynamic Programming/Grid/minPathSumInTriangle.dp.js
+++ Dynamic Programming/Grid/minPathSumInTriangle.dp.js
@@ -14,3 +14,31 @@ class Solution {
         return this.findMinPathSum(0, 0, tri)
     }
 }
+
+
+class Solution {
+    findMinPathSum(row, col, tri, memo) {
+        const m = tri.length
+        if (row === m - 1) return tri[row][col]
+
+        if (memo[row][col] !== -1) return memo[row][col]
+
+        const bottom = this.findMinPathSum(row + 1, col, tri, memo)
+        const bottomRight = this.findMinPathSum(row + 1, col + 1, tri, memo)
+
+        return memo[row][col] = tri[row][col] + Math.min(bottom, bottomRight)
+    }
+    minTriangleSum(tri) {
+        const m = tri.length
+        if (m === 1) return tri[0][0]
+
+        const memo = new Array(m).fill().map(() => [])
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < tri[i].length; j++) {
+                memo[i].push(-1)
+            }
+        }
+
+        return this.findMinPathSum(0, 0, tri, memo)
+    }
+}
