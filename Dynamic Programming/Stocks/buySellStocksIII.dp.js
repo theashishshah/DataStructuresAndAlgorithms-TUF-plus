7f8e6917b18d140728eb6fca8d8cf9d12e4de0bb
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 14:31:15 +0530

    best time to buy and sell stocks III using tabulation

--- Dynamic Programming/Stocks/buySellStocksIII.dp.js
+++ Dynamic Programming/Stocks/buySellStocksIII.dp.js
@@ -72,3 +72,28 @@ class Solution {
         return dp[0][1][2]
     }
 }
+
+
+class Solution {
+    stockBuySell(prices, n) {
+        if (n <= 1) return 0
+        const dp = Array(n + 1).fill().map(() => Array(2).fill().map(() => Array(3).fill(0)))
+        for (let index = n - 1; index >= 0; index--) {
+            for (let buy = 0; buy < 2; buy++) {
+                for (let cap = 0; cap < 3; cap++) {
+                    // buy == 1, you can buy
+                    if (buy === 1) {
+                        const buyToday = -prices[index] + dp[index + 1][0][cap]
+                        const skipToday = dp[index + 1][1][cap]
+                        dp[index][buy][cap] = Math.max(buyToday, skipToday)
+                    } else {
+                        const sellToday = cap > 0 ? prices[index] + dp[index + 1][1][cap - 1] : 0
+                        const skipToday = dp[index + 1][0][cap]
+                        dp[index][buy][cap] = Math.max(sellToday, skipToday)
+                    }
+                }
+            }
+        }
+        return dp[0][1][2]
+    }
+}
