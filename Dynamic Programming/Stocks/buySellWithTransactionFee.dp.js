3e746860f3a3166d25525c5386b886d4e4e696da
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 14:58:28 +0530

    max profit of buying and selling stocks with transaction fee.

--- Dynamic Programming/Stocks/buySellWithTransactionFee.dp.js
+++ Dynamic Programming/Stocks/buySellWithTransactionFee.dp.js
@@ -41,3 +41,30 @@ class Solution {
         return this.getMaxProfit(0, 1, fee, arr, n, dp)
     }
 }
+
+// using tabulation
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function (prices, fee) {
+    const n = prices.length
+    if (n <= 1) return 0
+    const tai = fee
+    const dp = Array(n + 1).fill().map(() => Array(2).fill(0))
+    for (let day = n - 1; day >= 0; day--) {
+        for (let buy = 0; buy < 2; buy++) {
+            if (buy === 1) {
+                const buyToday = -prices[day] + dp[day + 1][0]
+                const skipToday = dp[day + 1][1]
+                dp[day][buy] = Math.max(buyToday, skipToday)
+            } else {
+                const sellToday = -tai + prices[day] + dp[day + 1][1]
+                const skipToday = dp[day + 1][0]
+                dp[day][buy] = Math.max(sellToday, skipToday)
+            }
+        }
+    }
+    return dp[0][1]
+};
