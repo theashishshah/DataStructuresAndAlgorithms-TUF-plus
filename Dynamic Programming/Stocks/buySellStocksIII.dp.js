4eb7a0ccacc0a4e41c9bf4ad79e952e3eabd60df
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 08:56:13 +0530

    optimized using memoization method

--- Dynamic Programming/Stocks/buySellStocksIII.dp.js
+++ Dynamic Programming/Stocks/buySellStocksIII.dp.js
@@ -20,3 +20,30 @@ class Solution {
         return this.getMaxProfit(0, 1, 2, arr, n)
     }
 }
+
+
+//  optimized using memoization
+class Solution {
+    getMaxProfit(day, canBuy, cap, prices, n, dp) {
+        if (day >= n) return 0
+        if (cap === 0) return 0
+
+        if (dp[day][canBuy][cap] !== -1) return dp[day][canBuy][cap]
+        if (canBuy) {
+            // either I'll buy or I'll not buy
+            const buyToday = -prices[day] + this.getMaxProfit(day + 1, 0, cap, prices, n, dp)
+            const skipToday = this.getMaxProfit(day + 1, 1, cap, prices, n, dp)
+            return dp[day][canBuy][cap] = Math.max(buyToday, skipToday)
+        } else {
+            // I'll choose if i want to sell today or not
+            const sellToday = prices[day] + this.getMaxProfit(day + 1, 1, cap - 1, prices, n, dp)
+            const skipToday = this.getMaxProfit(day + 1, 0, cap, prices, n, dp)
+            return dp[day][canBuy][cap] = Math.max(sellToday, skipToday)
+        }
+    }
+    stockBuySell(arr, n) {
+        if (n <= 1) return 0
+        const dp = new Array(n).fill().map(() => Array(2).fill().map(() => Array(3).fill(-1)))
+        return this.getMaxProfit(0, 1, 2, arr, n, dp)
+    }
+}
