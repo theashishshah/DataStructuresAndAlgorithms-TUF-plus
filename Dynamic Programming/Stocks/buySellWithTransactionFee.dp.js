4637ef188176076d665130e6ebb6871edd71c781
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 14:47:32 +0530

    buying and selling of the stocks using with transaction fee.

--- Dynamic Programming/Stocks/buySellWithTransactionFee.dp.js
+++ Dynamic Programming/Stocks/buySellWithTransactionFee.dp.js
@@ -17,3 +17,27 @@ class Solution {
         return this.getMaxProfit(0, 1, fee, arr, n)
     }
 }
+
+
+// using memoization
+class Solution {
+    getMaxProfit(day, canBuy, tai, prices, n, dp) {
+        if (day >= n) return 0
+
+        if (dp[day][canBuy] !== -1) return dp[day][canBuy]
+        if (canBuy) {
+            const buyToday = -prices[day] + this.getMaxProfit(day + 1, 0, tai, prices, n, dp)
+            const skipToday = this.getMaxProfit(day + 1, 1, tai, prices, n, dp)
+            return dp[day][canBuy] = Math.max(buyToday, skipToday)
+        } else {
+            const sellToday = -tai + prices[day] + this.getMaxProfit(day + 1, 1, tai, prices, n, dp)
+            const skipToday = this.getMaxProfit(day + 1, 0, tai, prices, n, dp)
+            return dp[day][canBuy] = Math.max(sellToday, skipToday)
+        }
+    }
+    stockBuySell(arr, n, fee) {
+        if (n <= 1) return 0
+        const dp = Array(n).fill().map(() => Array(2).fill(-1))
+        return this.getMaxProfit(0, 1, fee, arr, n, dp)
+    }
+}
