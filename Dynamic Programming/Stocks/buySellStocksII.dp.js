a9375f208f7fd1d6df527db160476caa0eb30be5
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 05:49:54 +0530

    write more clean and bug free code

--- Dynamic Programming/Stocks/buySellStocksII.dp.js
+++ Dynamic Programming/Stocks/buySellStocksII.dp.js
@@ -1,15 +1,23 @@
 class Solution {
-    buySellHelper(day, buy, stocks, n) {
+    getMaxProfit(day, canBuy, prices, n) {
         if (day >= n) return 0
 
-        if (buy) {
-            return Math.max(-stocks[day] + this.buySellHelper(day + 1, !buy, stocks, n), this.buySellHelper(day + 1, buy, stocks, n))
+        if (canBuy) {
+            // if you have the option to buy, you can buy and loose money else don't buy
+            const buy = -prices[day] + this.getMaxProfit(day + 1, !canBuy, prices, n)
+            const skip = this.getMaxProfit(day + 1, canBuy, prices, n)
+            return Math.max(buy, skip)
         } else {
-            return Math.max(stocks[day] + this.buySellHelper(day + 1, !buy, stocks, n), this.buySellHelper(day + 1, buy, stocks, n))
+            // if you've option to sell, the you can decide if sell you'll get profit else nothing
+            const sell = prices[day] + this.getMaxProfit(day + 1, !canBuy, prices, n)
+            const skip = this.getMaxProfit(day + 1, canBuy, prices, n)
+            return Math.max(sell, skip)
         }
     }
     stockBuySell(arr, n) {
-        if (n === 1) return 0
-        return this.buySellHelper(0, true, arr, n)
+        // TC: O(2^n) as I'll always have the two options
+        // SC: O(n) recursion stack space
+        if (n <= 1) return 0
+        return this.getMaxProfit(0, true, arr, n)
     }
 }
