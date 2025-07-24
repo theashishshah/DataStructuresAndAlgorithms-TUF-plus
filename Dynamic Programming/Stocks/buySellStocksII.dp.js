66988b0a2bd842755005bbfd762565c3dc96fcc6
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 06:04:53 +0530

    memoization buy and sell stocks II

--- Dynamic Programming/Stocks/buySellStocksII.dp.js
+++ Dynamic Programming/Stocks/buySellStocksII.dp.js
@@ -21,3 +21,32 @@ class Solution {
         return this.getMaxProfit(0, true, arr, n)
     }
 }
+
+// using memoization
+function getMaxProfit(day, canBuy, prices, n, dpBuy, dpSell) {
+    if (day >= n) return 0;
+
+    if (canBuy && dpBuy[day] !== -1) return dpBuy[day];
+    if (!canBuy && dpSell[day] !== -1) return dpSell[day];
+
+    if (canBuy) {
+        // if you have the option to buy, you can buy and loose money else don't buy
+        const buyToday = -prices[day] + getMaxProfit(day + 1, false, prices, n, dpBuy, dpSell);
+        const skipToday = getMaxProfit(day + 1, true, prices, n, dpBuy, dpSell);
+        return dpBuy[day] = Math.max(buyToday, skipToday);
+    } else {
+        // if you've option to sell, the you can decide if sell you'll get profit else nothing
+        const sellToday = prices[day] + getMaxProfit(day + 1, true, prices, n, dpBuy, dpSell);
+        const skipToday = getMaxProfit(day + 1, false, prices, n, dpBuy, dpSell);
+        return dpSell[day] = Math.max(sellToday, skipToday);
+    }
+}
+
+var maxProfit = function (prices) {
+    const n = prices.length
+    if (n <= 1) return 0;
+
+    const dpBuy = new Array(n).fill(-1);
+    const dpSell = new Array(n).fill(-1);
+    return getMaxProfit(0, true, prices, n, dpBuy, dpSell);
+};
