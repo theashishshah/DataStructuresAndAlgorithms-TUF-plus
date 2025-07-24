a5d3a8af8c7a7fb0a5e5d7ae8ac97f294f5b3e19
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-24 23:44:34 +0530

    Finding subset whose sum is equal to target.

--- Dynamic Programming/Subsequences/findSubsetWhoseSumIsEqualToTarget.dp.js
+++ Dynamic Programming/Subsequences/findSubsetWhoseSumIsEqualToTarget.dp.js
@@ -11,3 +11,22 @@ class Solution {
         return this.findSubsetHelper(0, target, nums, n)
     }
 }
+
+// memoizatoin
+class Solution {
+    findSubsetHelper(index, target, nums, n, dp) {
+        if (target === 0) return true
+        if (index === n || target < 0) return false
+
+        if (dp[index][target] !== -1) return dp[index][target]
+
+        const take = this.findSubsetHelper(index + 1, target - nums[index], nums, n, dp)
+        const notTake = this.findSubsetHelper(index + 1, target, nums, n, dp)
+        return dp[index][target] = take || notTake
+    }
+    isSubsetSum(nums, target) {
+        const n = nums.length
+        const dp = Array(n).fill().map(() => Array(target + 1).fill(-1))
+        return this.findSubsetHelper(0, target, nums, n, dp)
+    }
+}
