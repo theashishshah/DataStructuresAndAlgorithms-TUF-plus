f673d55be234364256644edf60b0b884d7b5e60b
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-25 15:02:27 +0530

    Solve using memoization but still getting TLE, so gonna use tabulation

--- Dynamic Programming/Subsequences/minAbsSumDiff.dp.js
+++ Dynamic Programming/Subsequences/minAbsSumDiff.dp.js
@@ -30,3 +30,39 @@ class Solution {
         return min
     }
 }
+
+
+class Solution {
+    hasSubsetWithSum(currentIndex, remainingSum, numbers, dp) {
+        const n = numbers.length
+        if (remainingSum === 0) return true
+        if (currentIndex === n || remainingSum < 0) return false
+
+        if (dp[currentIndex][remainingSum] !== -1) return dp[currentIndex][remainingSum]
+        // include the current array's value
+        const include = this.hasSubsetWithSum(
+            currentIndex + 1, remainingSum - numbers[currentIndex],
+            numbers, dp
+        )
+
+        // don't include the current array's value
+        const exclude = this.hasSubsetWithSum(currentIndex + 1, remainingSum, numbers, dp)
+
+        return dp[currentIndex][remainingSum] = include || exclude
+    }
+    minDifference(arr, n) {
+        const sum = arr.reduce((pre, curr) => pre + curr, 0)
+        const targetSum = Math.floor(sum / 2)
+        let min = Infinity
+        for (let i = targetSum; i >= 0; i--) {
+            // If this targetSum exists then I'll substract this with total sum and
+            // this will be my min sum, else I'll try less
+            const dp = Array(n).fill().map(() => Array(i + 1).fill(-1))
+            if (this.hasSubsetWithSum(0, i, arr, dp)) {
+                const s2 = sum - i
+                min = Math.min(min, Math.abs(i - s2))
+            }
+        }
+        return min
+    }
+}
