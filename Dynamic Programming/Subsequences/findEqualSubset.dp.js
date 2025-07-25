ace49e159fcce6497df2aef145733cb8554c3313
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-25 06:20:32 +0530

    Find subset whose sum is equal using memoization.

--- Dynamic Programming/Subsequences/findEqualSubset.dp.js
+++ Dynamic Programming/Subsequences/findEqualSubset.dp.js
@@ -18,3 +18,29 @@ class Solution {
         return this.findEqualSubset(0, target / 2, nums, n)
     }
 }
+
+// using memoization
+class Solution {
+    findEqualSubset(index, target, nums, n, dp) {
+        if (target === 0) return true
+        if (index === n || target < 0) return false
+
+        if (dp[index][target] !== -1) return dp[index][target]
+
+        const take = this.findEqualSubset(index + 1, target - nums[index], nums, n, dp)
+        const notTake = this.findEqualSubset(index + 1, target, nums, n, dp)
+
+        return dp[index][target] = take || notTake
+    }
+
+    equalPartition(n, nums) {
+        let target = 0
+        for (let i = 0; i < n; i++) {
+            target += nums[i]
+        }
+        if (target % 2 !== 0) return false
+        target = target / 2
+        const dp = new Array(n).fill().map(() => Array(target + 1).fill(-1))
+        return this.findEqualSubset(0, target, nums, n, dp)
+    }
+}
