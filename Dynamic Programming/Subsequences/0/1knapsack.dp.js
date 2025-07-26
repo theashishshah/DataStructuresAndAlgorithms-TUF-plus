636ba8feda94ef0595edeced518621f9bbb62d79
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-26 23:50:06 +0530

    0/1 Knapsack using memoization.

--- Dynamic Programming/Subsequences/0/1knapsack.dp.js
+++ Dynamic Programming/Subsequences/0/1knapsack.dp.js
@@ -17,3 +17,27 @@ class Solution {
         return this.getMaxWeight(0, val, wt, W)
     }
 }
+
+
+// using memoization
+class Solution {
+    getMaxWeight(index, val, wt, bagSize, dp) {
+        const n = val.length
+        if (index === n - 1) {
+            if (bagSize >= wt[index]) return val[index]
+            else return 0
+        }
+        if (dp[index][bagSize] !== -1) return dp[index][bagSize]
+        let include = 0
+        if (bagSize >= wt[index]) {
+            include += val[index] + this.getMaxWeight(index + 1, val, wt, bagSize - wt[index], dp)
+        }
+        let exclude = this.getMaxWeight(index + 1, val, wt, bagSize, dp)
+
+        return dp[index][bagSize] = Math.max(include, exclude)
+    }
+    knapsack01(wt, val, n, W) {
+        const dp = Array(n).fill().map(() => Array(W + 1).fill(-1))
+        return this.getMaxWeight(0, val, wt, W, dp)
+    }
+}
