5ed844f208036289b6b43c6d2d3c649c9c750ddc
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 01:14:45 +0530

    Min number of coins using memoization technique.

--- Dynamic Programming/Subsequences/0/minCoins.dp.js
+++ Dynamic Programming/Subsequences/0/minCoins.dp.js
@@ -23,3 +23,33 @@ class Solution {
         else return ans
     }
 }
+
+// using memoization
+class Solution {
+    getMinCoins(index, coins, target, dp) {
+        if (target === 0) return 0
+        const n = coins.length
+        if (index === n - 1) {
+            // either I can take the last coin or not, if i can take then
+            // how many coins i can take?
+            if (target % coins[index] === 0) return target / coins[index]
+            else return 1e9 + 7
+        }
+
+        if (dp[index][target] !== -1) return dp[index][target]
+        let take = 1e9 + 7
+        if (target >= coins[index]) {
+            take = 1 + this.getMinCoins(index, coins, target - coins[index], dp)
+        }
+        let notTake = this.getMinCoins(index + 1, coins, target, dp)
+        if (take === notTake === 1e9 + 7) return 1e9 + 7
+        else return dp[index][target] = Math.min(take, notTake)
+    }
+    MinimumCoins(coins, amount) {
+        const n = coins.length
+        const dp = Array(n).fill().map(() => Array(amount + 1).fill(-1))
+        const ans = this.getMinCoins(0, coins, amount, dp)
+        if (ans >= 1e9 + 7) return -1
+        else return ans
+    }
+}
