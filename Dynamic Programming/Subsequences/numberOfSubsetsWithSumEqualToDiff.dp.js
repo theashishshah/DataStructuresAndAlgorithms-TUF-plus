a851b251d1416ec52fc59f9fd6cf1567c88541b5
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-26 23:47:25 +0530

    number of  subsets

---
+++ Dynamic Programming/Subsequences/numberOfSubsetsWithSumEqualToDiff.dp.js
@@ -0,0 +1,27 @@
+const sum = arr.reduce((pre, curr) => pre + curr, 0)
+const target = Math.floor(sum / 2)
+// let's space optimize it.
+let dp = Array(target + 1).fill(false)
+dp[0] = true
+
+for (let index = n - 1; index >= 0; index--) {
+    const curr = Array(target + 1).fill(false)
+    curr[0] = true
+    for (let rem = target; rem > 0; rem--) {
+        const include = rem >= arr[index] ? dp[rem - arr[index]] : false
+        const exclude = dp[rem]
+        curr[rem] = include || exclude
+    }
+    dp = curr
+}
+
+let count = 0
+for (let i = target; i >= 0; i--) {
+    if (dp[i]) {
+        // calculate s2 first because s1 is 'i'
+        const s2 = sum - i
+        if (Math.abs(i - s2) === diff) count++
+        
+    }
+}
+return count
