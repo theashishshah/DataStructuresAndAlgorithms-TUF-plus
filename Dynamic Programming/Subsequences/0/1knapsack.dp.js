3fe6aebbdc5aa529caa4954251d8c5437696c735
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 00:11:52 +0530

    0/1 knapsack using tabulation method.

--- Dynamic Programming/Subsequences/0/1knapsack.dp.js
+++ Dynamic Programming/Subsequences/0/1knapsack.dp.js
@@ -41,3 +41,36 @@ class Solution {
         return this.getMaxWeight(0, val, wt, W, dp)
     }
 }
+
+// using tabulation
+/**
+ * @param {number} W
+ * @param {number[]} val
+ * @param {number[]} wt
+ * @returns {number}
+ */
+
+class Solution {
+    knapsack(W, val, wt) {
+        // code here
+        const n = val.length
+        const dp = Array(n).fill().map(() => Array(W + 1).fill(0))
+
+        for (let cap = 0; cap <= W; cap++) {
+            // here i'm defining small bags, if these bags size is greater or equal to the weight of last item, then i'll take else not.
+            if (cap >= wt[n - 1]) dp[n - 1][cap] = val[n - 1]
+        }
+
+        for (let i = n - 2; i >= 0; i--) {
+            for (let j = 0; j <= W; j++) {
+                let include = 0
+                if (wt[i] <= j) {
+                    include = val[i] + dp[i + 1][j - wt[i]]
+                }
+                let exclude = dp[i + 1][j]
+                dp[i][j] = Math.max(exclude, include)
+            }
+        }
+        return dp[0][W]
+    }
+}
