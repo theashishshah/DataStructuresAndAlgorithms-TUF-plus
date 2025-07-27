7c2f18094870d0ccdf7f7f966100789c0c29f218
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 17:35:41 +0530

    Optimal solution for three sum problem.

--- Array/FAQ(medium)/3Sum.js
+++ Array/FAQ(medium)/3Sum.js
@@ -25,6 +25,7 @@ class Solution {
  *  so we need to find b + c, third element can we stored somewhere
  * 
  */
+// It is indeed better solution but not the optimal solution, we can optimize it more
 class Solution {
     threeSum(nums) {
         const n = nums.length
@@ -44,5 +45,38 @@ class Solution {
         // ["1,2,3", "3,5,3", "5,3,4"]
         return Array.from(ansSet).map(str => str.split(",").map(num => parseInt(num)))
 
+    }
+}
+
+// optimal solution
+class Solution {
+    /**
+    * @param number[] arr
+
+    * @returns number[][]
+    */
+    triplets(arr) {
+        // code here
+        const n = arr.length
+        const ans = []
+        arr.sort((a, b) => a - b)
+        for (let i = 0; i < n; i++) {
+            if (i > 0 && arr[i] === arr[i - 1]) continue
+            let left = i + 1
+            let right = n - 1
+            while (left < right) {
+                const sum = arr[i] + arr[left] + arr[right]
+                if (sum === 0) {
+                    ans.push([arr[i], arr[left], arr[right]])
+                    // avoid the left and right duplicates
+                    left++
+                    right--
+                    while (left < right && arr[left] === arr[left - 1]) left++
+                    while (left < right && arr[right] === arr[right + 1]) right--
+                } else if (sum < 0) left++
+                else right--
+            }
+        }
+        return ans
     }
 }
