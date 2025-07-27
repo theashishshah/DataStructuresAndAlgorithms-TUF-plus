9da0d4101c56a32447fe8ae7fa9c7b51eb400355
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 17:51:09 +0530

    Better 4 sum solution.

--- Array/FAQ(medium)/4Sum.js
+++ Array/FAQ(medium)/4Sum.js
@@ -21,3 +21,27 @@ class Solution {
         return Array.from(set).map(str => str.split(",").map(num => parseInt(num)))
     }
 }
+
+// slightly better solution from n^4 -> n^3
+class Solution {
+    fourSum(nums, target) {
+        const n = nums.length
+        const set = new Set()
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                const hash = new Set()
+                for (let k = j + 1; k < n; k++) {
+                    const fourth = target - (nums[i] + nums[j] + nums[k])
+                    if (hash.has(fourth)) {
+                        const temp = [fourth, nums[i], nums[j], nums[k]]
+                        temp.sort((a, b) => a - b)
+                        set.add(temp.join())
+                    }
+                    hash.add(nums[k])
+                }
+            }
+        }
+        // ["1,-2,24"] => [["1", "-2", "24"]]
+        return Array.from(set).map(str => str.split(",").map(num => parseInt(num)))
+    }
+}
