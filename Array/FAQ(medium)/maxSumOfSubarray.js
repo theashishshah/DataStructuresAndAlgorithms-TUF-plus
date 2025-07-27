7ecb549ca917028563b896bd965ddc1ff7bf36ca
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 20:52:28 +0530

    A better solution to find the max sum of sub-array.

--- Array/FAQ(medium)/maxSumOfSubarray.js
+++ Array/FAQ(medium)/maxSumOfSubarray.js
@@ -16,3 +16,22 @@ class Solution {
         return max
     }
 }
+
+// The problem above was each time we're calculating the sum, but now won't
+class Solution {
+    maxSubArray(nums) {
+        // TC: O(n^2)
+        // SC: O(1)
+        let max = -Infinity
+        const n = nums.length
+        for (let i = 0; i < n; i++) {
+            let localSum = 0
+            for (let j = i; j < n; j++) {
+                localSum += nums[j]
+                // this Each j, is forming a new sub-array, so I need to check
+                max = Math.max(localSum, max)
+            }
+        }
+        return max
+    }
+}
