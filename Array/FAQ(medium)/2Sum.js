a9148a8cdec64e354dab32ab59a1e7b15d987877
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 15:37:41 +0530

    Two sum better solution.

--- Array/FAQ(medium)/2Sum.js
+++ Array/FAQ(medium)/2Sum.js
@@ -10,3 +10,19 @@ class Solution {
         }
     }
 }
+// better solution:
+// approach: you're at current element, if you substract this num with target, you'll need the remaining ele, right? if you can store the prev whoever you've visited, then you can easily
+// get youre second element, right?
+class Solution {
+    twoSum(nums, target) {
+        const n = nums.length
+        const map = new Map()
+        for (let i = 0; i < n; i++) {
+            const curr = nums[i]
+            if (map.has(target - curr)) {
+                return [i, map.get(target - curr)]
+            }
+            map.set(curr, i)
+        }
+    }
+}
