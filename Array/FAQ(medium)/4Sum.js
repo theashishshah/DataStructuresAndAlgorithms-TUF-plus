84c11aa53e080a9794322c31d687df8359f9b741
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-27 18:03:47 +0530

    4 Sum optimal solution. use the same concept of 3 sum problem

--- Array/FAQ(medium)/4Sum.js
+++ Array/FAQ(medium)/4Sum.js
@@ -45,3 +45,31 @@ class Solution {
         return Array.from(set).map(str => str.split(",").map(num => parseInt(num)))
     }
 }
+
+class Solution {
+    fourSum(nums, target) {
+        const n = nums.length
+        nums.sort((a, b) => a - b)
+        const ans = []
+        for (let i = 0; i < n; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue
+            for (let j = i + 1; j < n; j++) {
+                if (j !== i + 1 && nums[j] === nums[j - 1]) continue
+                let left = j + 1
+                let right = n - 1
+                while (left < right) {
+                    const sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if (sum === target) {
+                        ans.push([nums[i], nums[j], nums[left], nums[right]])
+                        left++
+                        right--
+                        while (left < right && nums[left] === nums[left - 1]) left++
+                        while (left < right && nums[right] === nums[right + 1]) right--
+                    } else if (sum < target) left++
+                    else right--
+                }
+            }
+        }
+        return ans
+    }
+}
