d17c5d1942f0b5fc116639f1c57161effb8bcb27
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 13:51:29 +0530

    2 sum

---
+++ 0 Revision Series/03 Array/01 2-Sum.js
@@ -0,0 +1,11 @@
+class Solution {
+    twoSum(nums, target) {
+        const n = nums.length
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] === target) return [i, j]
+            }
+        }
+
+    }
+}
