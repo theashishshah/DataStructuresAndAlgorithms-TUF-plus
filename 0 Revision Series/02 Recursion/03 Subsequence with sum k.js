c80b2e1ad0ef6fa888bbdc2c6d1819362491a655
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 16:19:17 +0530

    Using dp

--- 0 Revision Series/02 Recursion/03 Subsequence with sum k.js
+++ 0 Revision Series/02 Recursion/03 Subsequence with sum k.js
@@ -14,3 +14,21 @@ class Solution {
         return this.backtrack(0, nums, k)
     }
 }
+
+class Solution {
+    backtrack(index, nums, target, dp) {
+        const n = nums.length
+        if (target === 0) return true
+        if (target < 0 || index === n) return false
+        if (dp[index][target]) return true
+        if (this.backtrack(index + 1, nums, target - nums[index], dp)) return dp[index][target] = true
+        if (this.backtrack(index + 1, nums, target, dp)) return dp[index][target] = true
+        return dp[index][target] = false
+
+    }
+    checkSubsequenceSum(nums, k) {
+        const n = nums.length
+        const dp = Array(n).fill().map(() => Array(k + 1).fill(false))
+        return this.backtrack(0, nums, k, dp)
+    }
+}
