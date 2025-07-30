9b0f4b630bf6f58e78feee189ede81fedb651208
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 15:00:53 +0530

    Optimal 4 sum solution.

--- 0 Revision Series/03 Array/03 4-Sum.js
+++ 0 Revision Series/03 Array/03 4-Sum.js
@@ -40,3 +40,32 @@ class Solution {
         return Array.from(set).map(str => str.split(",").map(num => Number(num)))
     }
 }
+
+class Solution {
+    fourSum(nums, target) {
+        const n = nums.length
+        const ans = []
+        nums.sort((a, b) => a - b)
+        for (let i = 0; i < n; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue
+            for (let j = i + 1; j < n; j++) {
+                if (j !== i + 1 && nums[j] === nums[j - 1]) continue
+                let left = j + 1
+                let right = n - 1
+                while (left < right) { // to avoid the duplicates
+                    const sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if (sum === target) {
+                        ans.push([nums[i], nums[j], nums[left], nums[right]])
+                        left++
+                        right--
+                        // now avoid the same elements using again
+                        while (left < right && nums[left] === nums[left - 1]) left++
+                        while (left < right && nums[right] === nums[right + 1]) right--
+                    } else if (sum < target) left++
+                    else right--
+                }
+            }
+        }
+        return ans
+    }
+}
