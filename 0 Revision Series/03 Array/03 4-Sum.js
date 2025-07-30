512c7595e07f3d39b72bcd6e39580a432c2208fd
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 14:51:12 +0530

    4 sum using hash map.

--- 0 Revision Series/03 Array/03 4-Sum.js
+++ 0 Revision Series/03 Array/03 4-Sum.js
@@ -17,3 +17,26 @@ function sum(nums, target) {
         return Array.from(set).map(str => str.split(",").map(num => Number(num)))
     }
 }
+
+
+class Solution {
+    fourSum(nums, target) {
+        const n = nums.length
+        const set = new Set()
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                const hash = new Set()
+                for (let k = j + 1; k < n; k++) {
+                    const next = target - (nums[i] + nums[j] + nums[k])
+                    if (hash.has(next)) {
+                        const ans = [nums[i], nums[j], nums[k], next]
+                        ans.sort((a, b) => a - b)
+                        set.add(ans.join())
+                    }
+                    hash.add(nums[k])
+                }
+            }
+        }
+        return Array.from(set).map(str => str.split(",").map(num => Number(num)))
+    }
+}
