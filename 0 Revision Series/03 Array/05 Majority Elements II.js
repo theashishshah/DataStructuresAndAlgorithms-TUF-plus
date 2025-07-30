55493c16eaddc1063cac99012843943f261f73b8
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 19:32:15 +0530

    Majority of elements brut force.

---
+++ 0 Revision Series/03 Array/05 Majority Elements II.js
@@ -0,0 +1,19 @@
+class Solution {
+    majorityElementTwo(nums) {
+        const n = nums.length
+        const ans = []
+        const min = Math.floor(n / 3)
+        for (let i = 0; i < n; i++) {
+            const curr = nums[i]
+            if (ans.length === 0 || ans[0] !== curr) {
+                let localCount = 0
+                for (let j = 0; j < n; j++) {
+                    if (curr === nums[j]) localCount++
+                }
+                if (localCount > min) ans.push(curr)
+            }
+            if (ans.length === 2) return ans
+        }
+        return ans
+    }
+}
