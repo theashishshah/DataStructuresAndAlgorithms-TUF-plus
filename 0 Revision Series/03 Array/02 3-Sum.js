2b8d24f9b4814e815df911a399f018340f4a5a56
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 14:16:10 +0530

    Using hash.

--- 0 Revision Series/03 Array/02 3-Sum.js
+++ 0 Revision Series/03 Array/02 3-Sum.js
@@ -16,3 +16,23 @@ class Solution {
         return Array.from(hash).map(str => str.split(",").map(num => Number(num)))
     }
 }
+
+class Solution {
+    threeSum(nums) {
+        const n = nums.length
+        const set = new Set()
+        for (let i = 0; i < n; i++) {
+            const hash = new Set()
+            for (let j = i + 1; j < n; j++) {
+                const next = -(nums[i] + nums[j])
+                if (hash.has(next)) {
+                    const curr = [next, nums[i], nums[j]]
+                    curr.sort((a, b) => a - b)
+                    set.add(curr.join())
+                }
+                hash.add(nums[j])
+            }
+        }
+        return Array.from(set).map(str => str.split(",").map(num => Number(num)))
+    }
+}
