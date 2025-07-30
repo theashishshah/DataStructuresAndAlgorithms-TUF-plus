78e5d88db7028c2529a505bfb20e1832f17a0a85
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 19:33:54 +0530

    Using set DS

--- 0 Revision Series/03 Array/05 Majority Elements II.js
+++ 0 Revision Series/03 Array/05 Majority Elements II.js
@@ -17,3 +17,21 @@ class Solution {
         return ans
     }
 }
+
+
+class Solution {
+    majorityElementTwo(nums) {
+        const n = nums.length
+        const ans = new Set()
+        const min = Math.floor(n / 3)
+        for (let i = 0; i < n; i++) {
+            const curr = nums[i]
+            let localCount = 0
+            for (let j = 0; j < n; j++) {
+                if (curr === nums[j]) localCount++
+            }
+            if (localCount > min) ans.add(curr)
+        }
+        return Array.from(ans)
+    }
+}
