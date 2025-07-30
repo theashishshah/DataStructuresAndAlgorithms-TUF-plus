54a989249bcbc3a4f54436538f41b570977524e6
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 20:01:19 +0530

    Using moore's voting algorithm concepts

--- 0 Revision Series/03 Array/05 Majority Elements II.js
+++ 0 Revision Series/03 Array/05 Majority Elements II.js
@@ -54,3 +54,38 @@ class Solution {
         return Array.from(ans)
     }
 }
+
+
+class Solution {
+    majorityElementTwo(nums) {
+        const n = nums.length
+        let count1 = 0, count2 = 0
+        let num1 = null
+        let num2 = null
+        for (let i = 0; i < n; i++) {
+            if (count1 === 0 && num2 !== nums[i]) {
+                count1++
+                num1 = nums[i]
+            } else if (count2 === 0 && num1 !== nums[i]) {
+                count2++
+                num2 = nums[i]
+            } else if (num1 === nums[i]) count1++
+            else if (num2 === nums[i]) count2++
+            else {
+                count1--
+                count2--
+            }
+        }
+        const ans = []
+        count1 = 0
+        count2 = 0
+        for (let i = 0; i < n; i++) {
+            if (nums[i] === num1) count1++
+            if (nums[i] === num2) count2++
+        }
+        const majority = Math.floor(n / 3)
+        if (count1 > majority) ans.push(num1)
+        if (count2 > majority) ans.push(num2)
+        return ans
+    }
+}
