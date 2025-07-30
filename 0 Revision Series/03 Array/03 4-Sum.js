fe209e7ebf534609f1919ae5ede42e11851cd22a
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-30 14:51:00 +0530

    4 sum worst tc

---
+++ 0 Revision Series/03 Array/03 4-Sum.js
@@ -0,0 +1,19 @@
+function sum(nums, target) {
+    const n = nums.length
+    const set = new Set()
+    for (let i = 0; i < n; i++){
+        for (let j = i + 1; j < n; j++){
+            for (let k = j + 1; k < n; k++){
+                for (let l = k + 1; l < n; l++){
+                    const sum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if (sum === target) {
+                        const curr = [nums[i], nums[j], nums[k], nums[l]]
+                        curr.sort((a, b) => a - b)
+                        set.add(curr)
+                    }
+                }
+            }
+        }
+        return Array.from(set).map(str => str.split(",").map(num => Number(num)))
+    }
+}
