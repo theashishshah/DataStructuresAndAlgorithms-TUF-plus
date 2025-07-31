200d55ddba110889f369bc385c22e162ae6ce67c
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-01 04:35:15 +0530

    Striver next greater element.

--- 0 Revision Series/04 Stack-Queue/06 Next greater element.js
+++ 0 Revision Series/04 Stack-Queue/06 Next greater element.js
@@ -16,3 +16,26 @@ class Solution {
         return ans
     }
 }
+
+class Solution {
+    nextLargerElement(arr) {
+        const n = arr.length
+        const ans = new Array(n).fill(-1)
+        const stack = []
+        for (let i = n - 1; i >= 0; i--) {
+            const curr = arr[i]
+            let len = stack.length
+            while (len > 0 && stack[len - 1] <= curr) {
+                stack.pop()
+                len--
+            }
+            // Now I'm sure if stack is not empty, I've my NGE 
+            len = stack.length
+            if (len > 0) {
+                ans[i] = stack[len - 1]
+            }
+            stack.push(curr)
+        }
+        return ans
+    }
+}
