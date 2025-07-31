539e7274deb05f89d9509b5980a323c162afd296
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-31 18:08:26 +0530

    Optimized approach.

--- 0 Revision Series/03 Array/10 Max product.js
+++ 0 Revision Series/03 Array/10 Max product.js
@@ -29,3 +29,23 @@ class Solution {
         return max
     }
 }
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    const n = nums.length
+    // Use suffix and prefix method
+    let suff = 1, pre = 1
+    let max = -Infinity
+    for (let i = 0; i < n; i++) {
+        if (suff === 0) suff = 1
+        if (pre === 0) pre = 1
+
+        pre *= nums[i]
+        suff *= nums[n - i - 1]
+        max = Math.max(max, Math.max(pre, suff))
+    }
+    return max
+};
