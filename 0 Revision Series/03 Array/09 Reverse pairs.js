cae7b95e25f0333242c8c40250bb786a2be0048f
Ashish Shah <eiepixxstuff@gmail.com>
2025-07-31 17:35:16 +0530

    Oprimized solution.

--- 0 Revision Series/03 Array/09 Reverse pairs.js
+++ 0 Revision Series/03 Array/09 Reverse pairs.js
@@ -10,3 +10,52 @@ class Solution {
         return count
     }
 }
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function merge(nums, low, mid, high) {
+    let left = low
+    let right = mid + 1
+    let count = 0
+    while (left <= mid && right <= high) {
+        if (nums[left] > 2 * nums[right]) {
+            count += (mid - left + 1)
+            right++
+        } else left++
+    }
+
+    const temp = []
+    left = low
+    right = mid + 1
+    while (left <= mid && right <= high) {
+        if (nums[left] <= nums[right]) temp.push(nums[left++])
+        else temp.push(nums[right++])
+    }
+
+    while (left <= mid) temp.push(nums[left++])
+    while (right <= high) temp.push(nums[right++])
+
+    for (let i = low; i <= high; i++) {
+        nums[i] = temp[i - low]
+    }
+
+    return count
+}
+function helper(nums, low, high) {
+    if (low >= high) return 0
+    let count = 0
+    const mid = Math.floor((low + high) / 2)
+    count += helper(nums, low, mid)
+    count += helper(nums, mid + 1, high)
+    count += merge(nums, low, mid, high)
+    return count
+}
+
+var reversePairs = function (nums) {
+    const n = nums.length
+    return helper(nums, 0, n - 1)
+};
