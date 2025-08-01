62dc80c8150ea214c2b8926a9fc282c49fe53988
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-01 11:05:09 +0530

    Optimized next greater element II

--- 0 Revision Series/04 Stack-Queue/07 Next greater element II.js
+++ 0 Revision Series/04 Stack-Queue/07 Next greater element II.js
@@ -14,3 +14,30 @@ class Solution {
         return ans
     }
 }
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function (nums) {
+    const n = nums.length
+    const ans = Array(n).fill(-1)
+    const stack = []
+    for (let i = 2 * n - 1; i >= 0; i--) {
+        const curr = nums[i % n]
+        let len = stack.length
+        while (len > 0 && stack[len - 1] <= curr) {
+            stack.pop()
+            len--
+        }
+
+        // Update to real index
+        if (i < n) {
+            len = stack.length
+            if (len > 0) {
+                ans[i] = stack[len - 1]
+            }
+        }
+        stack.push(curr)
+    }
+    return ans
+};
