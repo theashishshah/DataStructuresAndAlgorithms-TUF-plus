1aa9fe4110ee31073e617c28f79dc2af675f4073
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-01 22:18:29 +0530

    Max sliding window.

--- 0 Revision Series/04 Stack-Queue/Hard/02 Max sliding window.js
+++ 0 Revision Series/04 Stack-Queue/Hard/02 Max sliding window.js
@@ -12,3 +12,31 @@ class Solution {
         return ans
     }
 }
+
+class Solution {
+    maxSlidingWindow(nums, k) {
+        const n = nums.length
+        const ans = []
+        const dq = []
+        for (let i = 0; i < n; i++) {
+            // Maintain the current window
+            if (dq.length > 0 && dq[0] <= (i - k)) {
+                dq.shift()
+            }
+
+            // maintain the monotonicity(decreasing order)
+            while (dq.length > 0 && nums[dq[dq.length - 1]] <= nums[i]) {
+                dq.pop()
+            }
+
+            // push into queue
+            dq.push(i)
+
+            // take the largerst of the current window
+            if (i >= (k - 1)) {
+                ans.push(nums[dq[0]])
+            }
+        }
+        return ans
+    }
+}
