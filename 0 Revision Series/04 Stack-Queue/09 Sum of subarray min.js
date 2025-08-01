52f6205ec27ad0c0235cea50e3d8e3e87af48cc0
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-01 13:47:20 +0530

    Sum of subarray's mins which i don't understand on the first go, but later I'll

--- 0 Revision Series/04 Stack-Queue/09 Sum of subarray min.js
+++ 0 Revision Series/04 Stack-Queue/09 Sum of subarray min.js
@@ -30,3 +30,56 @@ var sumSubarrayMins = function (nums) {
     }
     return sum % (1e9 + 7)
 };
+
+class Solution {
+    findNSE(arr) {
+        const n = arr.length
+        const ans = new Array(n).fill(0)
+        const stack = []
+        for (let i = n - 1; i >= 0; i--) {
+            const curr = arr[i]
+            let len = stack.length
+            while (len > 0 && arr[stack[len - 1]] >= curr) {
+                stack.pop()
+                len--
+            }
+            len = stack.length
+            ans[i] = len > 0 ? stack[len - 1] : n
+            stack.push(i)
+        }
+        return ans
+    }
+
+    findPSEE(arr) {
+        const n = arr.length
+        const ans = new Array(n).fill(0)
+        const stack = []
+        for (let i = 0; i < n; i++) {
+            const curr = arr[i]
+            let len = stack.length
+            while (len > 0 && arr[stack[len - 1]] > curr) {
+                stack.pop()
+                len--
+            }
+            len = stack.length
+            ans[i] = len > 0 ? stack[len - 1] : -1
+            stack.push(i)
+        }
+        return ans
+    }
+    sumSubarrayMins(arr) {
+        const nseIndices = this.findNSE(arr)
+        const pseeIndices = this.findPSEE(arr)
+        const n = arr.length
+        let sum = 0
+        for (let i = 0; i < n; i++) {
+            const left = i - pseeIndices[i]
+            const right = nseIndices[i] - i
+            const freq = left * right
+            const value = (arr[i] * freq) % (1e9 + 7)
+            sum += value
+            sum = sum % (1e9 + 7)
+        }
+        return sum
+    }
+}
