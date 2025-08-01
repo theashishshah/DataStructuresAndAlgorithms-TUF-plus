ef029530b0fa523d64f616445d9c1b8c82feddbc
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-01 16:53:05 +0530

    Optimized approach.

--- 0 Revision Series/04 Stack-Queue/Hard/01 Min stack.js
+++ 0 Revision Series/04 Stack-Queue/Hard/01 Min stack.js
@@ -24,3 +24,46 @@ class MinStack {
         return min
     }
 }
+
+
+
+var MinStack = function () {
+    this.stack = [] // [[value, min], [value, min]]
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+    if (this.stack.length === 0) {
+        this.stack.push([val, val])
+        return
+    }
+
+    let min = Math.min(this.getMin(), val)
+    this.stack.push([val, min])
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    let len = this.stack.length
+    return this.stack[len - 1][0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    let len = this.stack.length
+    return this.stack[len - 1][1]
+};
