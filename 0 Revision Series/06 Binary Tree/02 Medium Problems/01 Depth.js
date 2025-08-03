cbf3bdbbf55c7afa8cccc89bf86748dacdbd349e
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-03 14:15:36 +0530

    Using level order traversal.

--- 0 Revision Series/06 Binary Tree/02 Medium Problems/01 Depth.js
+++ 0 Revision Series/06 Binary Tree/02 Medium Problems/01 Depth.js
@@ -9,3 +9,23 @@ class Solution {
         return this.height(root)
     }
 }
+
+
+class Solution {
+    maxDepth(root) {
+        if (!root) return 0
+        let depth = 0
+        const queue = []
+        queue.push(root)
+        while (queue.length) {
+            const len = queue.length
+            for (let i = 0; i < len; i++) {
+                const curr = queue.shift()
+                if (curr.left) queue.push(curr.left)
+                if (curr.right) queue.push(curr.right)
+            }
+            depth++
+        }
+        return depth
+    }
+}
