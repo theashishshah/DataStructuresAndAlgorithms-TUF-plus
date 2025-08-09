468df8f173f31be3de5e0105bc7f105397dae50e
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-09 20:14:12 +0530

    Optimized aaproach of nearest.

--- 0 Revision Series/07 Graph/01 Traversal Problems/05 Nearest distance in Binary matrix.js
+++ 0 Revision Series/07 Graph/01 Traversal Problems/05 Nearest distance in Binary matrix.js
@@ -4,6 +4,8 @@ class Solution {
         const cols = grid[0].length
 
         // Get all the 1's so that in each step, you spread them like flue
+        // TC: 5O(n*m)
+        // SC: 2xO(n*m) + O(n*m)
         const visited = new Array(rows).fill().map(() => Array(cols).fill(false))
         const ans = Array.from({ length: rows }, () => Array(cols).fill(0))
         const queue = []
@@ -41,3 +43,50 @@ class Solution {
     }
 }
 
+
+// Optimised solution
+// User function Template for javascript
+
+/**
+ * @param {number[][]} grid
+ * @returns {number[][]}
+ */
+class Solution {
+    // Function to find distance of nearest 1 in the grid for each cell.
+    nearest(grid) {
+        // code here
+        const rows = grid.length
+        const cols = grid[0].length
+
+        // Try to find at least 1
+        const ans = Array.from({ length: rows }, () => Array(cols).fill(-1))
+        // TC: 
+        // SC: O(m*n) + O(n*m)
+        const queue = []
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (grid[i][j] === 1) {
+                    ans[i][j] = 0
+                    queue.push([i, j])
+                }
+            }
+        }
+        let head = 0
+        const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while (head < queue.length) {
+            const [row, col] = queue[head++]
+            for (const [dx, dy] of directions) {
+                const nextrow = row + dx
+                const nextcol = col + dy
+                if (nextrow >= 0 && nextrow < rows &&
+                    nextcol >= 0 && nextcol < cols &&
+                    ans[nextrow][nextcol] === -1
+                ) {
+                    ans[nextrow][nextcol] = ans[row][col] + 1
+                    queue.push([nextrow, nextcol])
+                }
+            }
+        }
+        return ans
+    }
+}
