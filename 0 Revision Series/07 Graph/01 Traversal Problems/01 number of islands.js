286100d446d54ddf115645e3106b236a5d8ec011
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-09 14:45:27 +0530

    number of islands using bfs.

--- 0 Revision Series/07 Graph/01 Traversal Problems/01 number of islands.js
+++ 0 Revision Series/07 Graph/01 Traversal Problems/01 number of islands.js
@@ -35,3 +35,49 @@ class Solution {
         return count
     }
 }
+
+
+
+
+// using bfs
+class Solution {
+    bfs(row, col, visited, grid) {
+        const rows = grid.length
+        const cols = grid[0].length
+        const queue = []
+        queue.push([row, col])
+        while (queue.length) {
+            const [crow, ccol] = queue.shift()
+            // Traverse its neighbours
+            for (let i = -1; i <= 1; i++){
+                for (let j = -1; j <= 1; j++){
+                    const nextRow = crow + i
+                    const nextCol = ccol + j
+                    if (nextRow >= 0 && nextRow < rows
+                        && nextCol >= 0 && nextCol < cols
+                        && grid[nextRow][nextCol] === "1" && !visited[nextRow][nextCol]){
+                        visited[nextRow][nextCol] = true
+                        queue.push([nextRow, nextCol])
+                        }
+                }
+            }
+        }
+    }
+    numIslands(grid) {
+        const rows = grid.length
+        const cols = grid[0].length
+        const visited = Array.from({ length: rows }, () => Array(cols).fill(false))
+
+        let count = 0
+        for (let i = 0; i < rows; i++){
+            for (let j = 0; j < cols; j++){
+                if (grid[i][j] === "1" && !visited[i][j]) {
+                    visited[i][j] = true
+                    count++
+                    this.bfs(i, j, visited, grid)
+                }
+            }
+        }
+        return count
+    }
+}
