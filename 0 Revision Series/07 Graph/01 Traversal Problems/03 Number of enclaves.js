271c6b5b27a2238432a22dc35cf2b52f03567780
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-09 15:49:31 +0530

    Finding the number of enclaves using dfs.

--- 0 Revision Series/07 Graph/01 Traversal Problems/03 Number of enclaves.js
+++ 0 Revision Series/07 Graph/01 Traversal Problems/03 Number of enclaves.js
@@ -44,3 +44,58 @@ class Solution {
     }
 
 }
+
+
+
+// using dfs
+// User function Template for javascript
+/**
+ * @param {number[][]} grid
+ * @returns {number}
+ */
+
+class Solution {
+    constructor() {
+        this.directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    }
+    dfs(row, col, visited, grid) {
+        const rows = grid.length
+        const cols = grid[0].length
+        visited[row][col] = true
+        // go in depth
+        for (const [dx, dy] of this.directions) {
+            const nextrow = row + dx
+            const nextcol = col + dy
+            if (nextrow >= 0 && nextrow < rows && nextcol >= 0 && nextcol < cols && !visited[nextrow][nextcol]
+                && grid[nextrow][nextcol] === 1
+            ) {
+                this.dfs(nextrow, nextcol, visited, grid)
+            }
+        }
+    }
+    // Function to count the number of enclaves.
+    numberOfEnclaves(grid) {
+        // your code here
+        const rows = grid.length
+        const cols = grid[0].length
+
+        // start from the  boundary itself
+        const visited = new Array(rows).fill().map(() => Array(cols).fill(false))
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if ((i === 0 || i === rows - 1 || j === 0 || j === cols - 1) && grid[i][j] === 1) {
+                    this.dfs(i, j, visited, grid)
+                }
+            }
+        }
+
+        let count = 0
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (!visited[i][j] && grid[i][j]) count++
+            }
+        }
+        return count
+
+    }
+}
